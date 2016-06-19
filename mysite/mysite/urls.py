7eaa3419723f8a9759f162510c7f6c7788515bf3
amkk <aleksandra.m.kopec@gmail.com>
2016-06-19 17:29:18 +0200

    Dodano widok i szablon dla poszczególnego wpisu na stronie

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^blogdjango/', include('blogdjango.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
